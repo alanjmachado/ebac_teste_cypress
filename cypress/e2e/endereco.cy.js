06aa6fda6b897c0aec299585adeebeddb584ccc9
alan.machado <alan.machado@efrete.com.br>
2023-04-13 10:59:17 -0300

    Utilizando Page Objects

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,5 @@
 /// <reference types="cypress" />
+import EnderecoPage from '../support/page-objects/endereco.page'
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,7 +11,7 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     });
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-
-        //cadastro de endereço
+        EnderecoPage.editarEnderecoFaturamento('Lucio', 'Master', 'BlackRock', 'Brasil', 'Rua Adolfo Jose de Assis', '1105', 'Itajai','Santa Catarina', '88303530', '47999999999', 'alan@gmail.com')
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')        
     });
 });
