72949d683a4aa2be0cb1ed38f95391ea5cf287a7
alan.machado <alan.machado@efrete.com.br>
2023-04-17 09:51:07 -0300

    Massa de dados em lista

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,5 +1,6 @@
 /// <reference types="cypress" />
 import EnderecoPage from '../support/page-objects/endereco.page'
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -12,6 +13,23 @@ describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
 
     it('Deve fazer cadastro de faturamento com sucesso', () => {
         EnderecoPage.editarEnderecoFaturamento('Lucio', 'Master', 'BlackRock', 'Brasil', 'Rua Adolfo Jose de Assis', '1105', 'Itajai','Santa Catarina', '88303530', '47999999999', 'alan@gmail.com')
+        cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')        
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        EnderecoPage.editarEnderecoFaturamento(
+            dadosEndereco[1].nome,
+            dadosEndereco[1].sobrenome,
+            dadosEndereco[1].empresa,
+            dadosEndereco[1].pais,
+            dadosEndereco[1].endereço,
+            dadosEndereco[1].numero,
+            dadosEndereco[1].cidade,
+            dadosEndereco[1].estado,
+            dadosEndereco[1].cep,
+            dadosEndereco[1].telefone,
+            dadosEndereco[1].email
+            )
         cy.get('.woocommerce-message').should('contain' , 'Endereço alterado com sucesso.')        
     });
 });
